c898405e8f33f90c9b3435dd1b64bf9b6db39c3b
Rohan Kar <realrohankar03@gmail.com>
2024-02-11 14:14:49 +0530

    props testing

--- Week four/frontend-todo-app/src/App.jsx
+++ Week four/frontend-todo-app/src/App.jsx
@@ -1,26 +1,46 @@
+import { useState } from 'react'
 import './App.css'
 
-var todos =[{
-  title:"Have Dinner",
-  description: "Dinner from 6-7 pm",
-  id: 1
-},
-{
-  title:"Go gym",
-  description: "Gym from 8-9 pm",
-  id: 1
-}
-]
+// var todos =[{
+//   
+// }
+// ]
 
 //Component
 function App() {
 
+  const [todo,setTodo] = useState({
+    
+      title:"Go gym",
+      description: "Gym from 8-9 pm",
+      id: 1
+    });
+
+    setInterval(()=>{
+      setTodo({
+        title:"Have Dinner",
+        description: "Dinner from 6-7 pm",
+        id: 1
+      })
+    },2000)
+
   return (
     <>
       <h1>Hi There</h1>
-      {"Title: "+ todos[0].title}
+      {todo.title}
+      {todo.description}
+      <PersonName firstName = {"Morgan"} lastName ={"Kar"}></PersonName>
     </>
   )
 }
 
+function PersonName(props){
+  return <>
+  <div>
+    {props.firstName} {props.lastName}
+  </div>
+  </>
+}
+
 export default App
+
