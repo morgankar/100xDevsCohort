759604146543553b0d9c662a74a426c19510d566
Rohan Kar <realrohankar03@gmail.com>
2024-02-11 07:36:27 +0530

    changed content of app.jsx

--- Week four/frontend-todo-app/src/App.jsx
+++ Week four/frontend-todo-app/src/App.jsx
@@ -1,11 +1,24 @@
-
 import './App.css'
 
+var todos =[{
+  title:"Have Dinner",
+  description: "Dinner from 6-7 pm",
+  id: 1
+},
+{
+  title:"Go gym",
+  description: "Gym from 8-9 pm",
+  id: 1
+}
+]
+
+//Component
 function App() {
 
   return (
     <>
       <h1>Hi There</h1>
+      {"Title: "+ todos[0].title}
     </>
   )
 }
