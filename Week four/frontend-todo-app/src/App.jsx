4b2d8b8fcf4ecf37ed58dd063144a02394cb94c7
Rohan Kar <realrohankar03@gmail.com>
2024-02-12 20:52:07 +0530

    basic todo

--- Week four/frontend-todo-app/src/App.jsx
+++ Week four/frontend-todo-app/src/App.jsx
@@ -1,46 +1,38 @@
 import { useState } from 'react'
 import './App.css'
 
-// var todos =[{
-//   
-// }
-// ]
 
 //Component
 function App() {
 
-  const [todo,setTodo] = useState({
-    
-      title:"Go gym",
-      description: "Gym from 8-9 pm",
-      id: 1
-    });
+ const [todos,setTodos] = useState([ {
+    title: "Go gym",
+    description: "Do gym from 7-8 pm",
+    id:1
+    },
+    {
+      title: "Have Dinner",
+      description: "Eat at 6pm",
+      id:2
+    }]);
 
-    setInterval(()=>{
-      setTodo({
-        title:"Have Dinner",
-        description: "Dinner from 6-7 pm",
-        id: 1
-      })
-    },2000)
-
-  return (
+   return (
     <>
-      <h1>Hi There</h1>
-      {todo.title}
-      {todo.description}
-      <PersonName firstName = {"Morgan"} lastName ={"Kar"}></PersonName>
+    <div>
+      {todos.map((todo) => {
+        return <div>
+        {todo.id}
+        {todo.title}
+        {todo.description}
+        </div>
+      })}
+    </div>
     </>
-  )
+   )
+   
 }
 
-function PersonName(props){
-  return <>
-  <div>
-    {props.firstName} {props.lastName}
-  </div>
-  </>
-}
+
 
 export default App
 
